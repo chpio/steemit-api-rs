6b07ebabd5528a28a1bfecb9653cc31635d25355
Thomas Heck <t@b128.net>
2018-02-13 17:07:11 +0100

    fix pinning

--- dtube-pinner/main.rs
+++ dtube-pinner/main.rs
@@ -43,6 +43,7 @@ struct DtubeMetadata<'a> {
 pub struct Config<'a> {
     #[serde(borrow)]
     pub names: Vec<&'a str>,
+    pub concurrent_requests: usize,
     pub concurrent_pins: usize,
     pub interval_ms: u64,
     pub pin_snaphash: bool,
@@ -55,6 +56,7 @@ pub struct Config<'a> {
 pub enum Error {
     SteemIt(steemit_api::Error),
     Ipfs(ipfs_api::response::Error),
+    StdIo(std::io::Error),
 }
 
 fn main() {
@@ -63,19 +65,18 @@ fn main() {
         .read_to_end(&mut config)
         .expect("Couldn't load config");
     let config: Config = toml::from_slice(&config).expect("Couldn't parse config");
-    println!("config: {:?}", config);
     let mut core = Core::new().unwrap();
     let handle = core.handle();
     let api = Api::new("https://api.steemit.com/".parse().unwrap(), &handle);
     let ipfs = ipfs_api::IpfsClient::default(&handle);
-    let f = tokio_core::reactor::Interval::new_at(
+    let runner = tokio_core::reactor::Interval::new_at(
         std::time::Instant::now(),
         std::time::Duration::from_millis(config.interval_ms),
         &handle,
     ).unwrap()
-        .for_each(|_| {
+        .map_err(Error::StdIo)
+        .map(|_| {
             let name_futures = config.names.iter().map(|name| {
-                println!("loading: {:?}", name);
                 api.request(
                     [
                         &message::RequestGetDiscussionsByBlog {
@@ -84,55 +85,51 @@ fn main() {
                         },
                     ].as_ref(),
                 ).map_err(Error::SteemIt)
-                    .map(|res| {
-                        println!("foo");
-                        let pin_futures = res.0
-                            .into_iter()
-                            .filter_map(|entry| {
-                                if entry.category != "dtube" {
-                                    return None;
-                                }
-                                let meta: DtubeMetadata =
-                                    match serde_json::from_str(&entry.json_metadata) {
-                                        Ok(meta) => meta,
-                                        Err(err) => {
-                                            println!(
-                                                "Couln't deserialize dtube meta: `{:?}` err: `{}`",
-                                                entry.json_metadata, err
-                                            );
-                                            return None;
-                                        }
-                                    };
-                                let snaphash = bool_to_option(config.pin_snaphash)
-                                    .and_then(|_| str_to_option(meta.video.info.snaphash));
-                                let spritehash = bool_to_option(config.pin_spritehash)
-                                    .and_then(|_| str_to_option(meta.video.info.spritehash));
-                                let videohash = bool_to_option(config.pin_videohash)
-                                    .and_then(|_| str_to_option(meta.video.content.videohash));
-                                let video480hash = bool_to_option(config.pin_video480hash)
-                                    .and_then(|_| str_to_option(meta.video.content.video480hash));
-                                let pin_futures: Vec<_> =
-                                    [snaphash, spritehash, videohash, video480hash]
-                                        .into_iter()
-                                        .cloned()
-                                        .filter_map(|hash| hash)
-                                        .map(|hash| {
-                                            println!("pinning: {:?}", hash);
-                                            ipfs.pin_add(hash, true).map_err(Error::Ipfs)
-                                        })
-                                        .collect();
-                                Some(pin_futures)
-                            })
-                            .flat_map(|e| e);
-                        futures::stream::iter_ok(pin_futures)
-                    })
-                    .flatten_stream()
-                    .buffer_unordered(config.concurrent_pins)
-                    .for_each(|_| Ok(()))
             });
-            futures::stream::iter_ok(name_futures).for_each(|_| Ok(()))
-        });
-    core.run(f).unwrap();
+            futures::stream::iter_ok(name_futures)
+        })
+        .flatten()
+        .buffer_unordered(config.concurrent_requests)
+        .map(|res| {
+            let pin_futures = res.0
+                .into_iter()
+                .filter_map(|entry| {
+                    if entry.category != "dtube" {
+                        return None;
+                    }
+                    let meta: DtubeMetadata = match serde_json::from_str(&entry.json_metadata) {
+                        Ok(meta) => meta,
+                        Err(err) => {
+                            println!(
+                                "Couldn't deserialize dtube meta: `{:?}` err: `{}`",
+                                entry.json_metadata, err
+                            );
+                            return None;
+                        }
+                    };
+                    let snaphash = bool_to_option(config.pin_snaphash)
+                        .and_then(|_| str_to_option(meta.video.info.snaphash));
+                    let spritehash = bool_to_option(config.pin_spritehash)
+                        .and_then(|_| str_to_option(meta.video.info.spritehash));
+                    let videohash = bool_to_option(config.pin_videohash)
+                        .and_then(|_| str_to_option(meta.video.content.videohash));
+                    let video480hash = bool_to_option(config.pin_video480hash)
+                        .and_then(|_| str_to_option(meta.video.content.video480hash));
+                    let pin_futures: Vec<_> = [snaphash, spritehash, videohash, video480hash]
+                        .into_iter()
+                        .cloned()
+                        .filter_map(|hash| hash)
+                        .map(|hash| ipfs.pin_add(hash, true).map_err(Error::Ipfs))
+                        .collect();
+                    Some(pin_futures)
+                })
+                .flat_map(|e| e);
+            futures::stream::iter_ok(pin_futures)
+        })
+        .flatten()
+        .buffer_unordered(config.concurrent_pins)
+        .for_each(|_| Ok(()));
+    core.run(runner).unwrap();
 }
 
 fn str_to_option(s: &str) -> Option<&str> {
